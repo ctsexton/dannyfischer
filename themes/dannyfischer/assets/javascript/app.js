61d77034c10089e95f423f1a45c730f90762e62c
ctsexton <cameron.t.sexton@gmail.com>
2018-05-31 02:27:30 +1000

    develop theme

---
+++ themes/dannyfischer/assets/javascript/app.js
@@ -0,0 +1,21 @@
+function setupWindow() {
+	var scroller = function (button, element) {
+	  $(button).on('click', function() {
+		$(element).get(0).scrollIntoView({
+		  block: "start",
+		  behavior: "smooth"
+		});
+	  });
+
+	}
+	$(document).ready(function() {
+
+		scroller('#shows', '#showdiv');
+		scroller('#bio', '#biodiv');
+		scroller('#recs', '#recdiv');
+		scroller('#contact', '#contactdiv');
+
+	})
+}
+
+window.setupWindow();
