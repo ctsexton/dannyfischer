e1c36509215cbeea6fad292db1bf43bc3c427fcf
ctsexton <cameron.t.sexton@gmail.com>
2018-06-01 01:18:10 +1000

    develop theme

--- themes/dannyfischer/assets/javascript/app.js
+++ themes/dannyfischer/assets/javascript/app.js
@@ -1,22 +1,41 @@
-function setupWindow() {
+function setupWebpage() {
+
 	var scroller = function (button, element) {
-	  $(button).on('click', function() {
-		$(element).get(0).scrollIntoView({
-		  block: "start",
-		  behavior: "smooth"
-		});
-	  });
-
+		$(button).on("click", function(event){
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $(element).position().top
+        }, 500, 'swing');
+});
 	}
+
+	var removeClass = function (buttonId, elementId, toAdd) {
+		$(buttonId).on('click', function() {
+			$(elementId).removeClass(toAdd);
+		});
+	}
+	var addClass = function (buttonId, elementId, toRemove) {
+		 $(buttonId).on('click', function() {
+			 $(elementId).addClass(toRemove);
+		 });
+	 }
+
+	var elements = [
+		['#menu--title', '#topdiv'],
+		['#shows', '#showdiv'],
+		['#bio', '#biodiv'],
+		['#recs', '#recdiv'],
+		['#contact', '#contactdiv']
+	];
+
 	$(document).ready(function() {
+		elements.map(function (element) {
+			scroller(element[0], element[1]);
+			removeClass(element[0], '#menu--list', 'menu--mobile__open');
+		});
 
-		scroller('#top', '#topdiv');
-		scroller('#shows', '#showdiv');
-		scroller('#bio', '#biodiv');
-		scroller('#recs', '#recdiv');
-		scroller('#contact', '#contactdiv');
-
-	})
+		addClass('#hamburger', '#menu--list', 'menu--mobile__open');
+	});
 }
 
-window.setupWindow();
+window.setupWebpage();
