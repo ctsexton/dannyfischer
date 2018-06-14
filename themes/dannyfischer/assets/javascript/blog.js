690581f204946acd7d0e6b2e65dcbd3d19aae944
ctsexton <cameron.t.sexton@gmail.com>
2018-06-14 14:03:28 +1000

    finish first draft simple responsive design blog

--- themes/dannyfischer/assets/javascript/blog.js
+++ themes/dannyfischer/assets/javascript/blog.js
@@ -16,7 +16,37 @@ $(function () {
 
 	elements.map(function(elem) {
 		$(elem[0]).attr("href", '/' + elem[1]);
-		console.log('elem ' + elem[0] + ' mapped to ' + elem[1]);
+		//console.log('elem ' + elem[0] + ' mapped to ' + elem[1]);
 	});
 	addClass('#hamburger', '#menu--list', 'menu--mobile__open');
+
+	var menuState = 0; // 0 for closed, 1 open
+	
+	// when element buttonId is clicked, class toRemove is removed from elementId
+	var removeClass = function (buttonId, elementId, toRemove) {
+		$(buttonId).on('click', function() {
+			$(elementId).removeClass(toRemove);
+		});
+	}
+
+	// when element buttonId is clicked, class toAdd is added to elementId
+	var addClass = function (buttonId, elementId, toAdd) {
+		 $(buttonId).on('click', function() {
+			 $(elementId).addClass(toAdd);
+		 });
+	}
+
+	$('#latest-posts--button').on('click', function() {
+		if (menuState == 0) {
+			$('#latest-posts--list').removeClass('blog--post-list__closed');
+			$('#latest-posts--list').addClass('blog--post-list__open');
+			$('#latest-posts--arrow').html('&#x25BE');
+			menuState = 1;
+		} else {
+			$('#latest-posts--list').addClass('blog--post-list__closed');
+			$('#latest-posts--list').removeClass('blog--post-list__open');
+			$('#latest-posts--arrow').html('&#x25C2');
+			menuState = 0;
+		}
+	});
 });
